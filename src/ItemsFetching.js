5cfbe708df17ff3fbf9c4860abad12a96f359e64
Tallberg Topias <topias.tallberg@gmail.com>
2022-03-08 12:00:38 +0200

    changed list to antD list

--- src/ItemsFetching.js
+++ src/ItemsFetching.js
@@ -1,28 +1,35 @@
 import React, { useEffect } from "react"
 import axios from 'axios'
 import { Link } from "react-router-dom"
+import { List } from 'antd';
 
 function ItemsFetching(props) {
 
-    useEffect(() => {
-        axios.get('https://test.tpfons.fi/tpdemo/ext/back/api/v1-0000/donation/targets')
-        .then(res => {
-            props.setData(res.data.giftTargets)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+  useEffect(() => {
+    axios.get('https://test.tpfons.fi/tpdemo/ext/back/api/v1-0000/donation/targets')
+    .then(res => {
+      props.setData(res.data.giftTargets)
     })
+    .catch(error => {
+      console.log(error)
+    })
+  })
     return (
-        <div>
-            <ul>
-                {props.data.map(data => (
-                    <li key={data.giftTargetId}>
-                         <Link to={`/giftTarget/${data.giftTargetId}`}>{data.name} </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div>
+        <ul>
+          <List
+            bordered
+            dataSource={props.data}
+            renderItem={item => (
+              <List.Item>
+                <Link to={`/giftTarget/${item.giftTargetId}`}>
+                  {item.name}
+                </Link>
+              </List.Item>
+            )}
+          />
+        </ul>
+      </div>
     )
 }
 
